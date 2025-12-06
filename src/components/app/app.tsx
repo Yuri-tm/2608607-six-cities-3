11dffb370aeefb99d969532c5951e6257206d7da
Yuri-tm <yuritm.tm@gmail.com>
2025-12-06 20:19:46 +0300

    начинает маршрутизацию

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
-import MainPage from '../../pages/main/main';
+import MainPage from '../../pages/main/main-page';
+import FavoritesPage from '../../pages/favorites/favorites-page';
+import LoginPage from '../../pages/login/login-page';
+import OfferPage from '../../pages/offer/offer-page';
+import ErrorPage from '../../pages/error-page/error-page';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 type AppScreenProps = {
   locationCount: number;
@@ -6,6 +12,14 @@ type AppScreenProps = {
 
 export default function App({locationCount}: AppScreenProps): JSX.Element {
   return (
-    <MainPage locationCount={locationCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage locationCount={locationCount} />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/offer/:id" element={<OfferPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
